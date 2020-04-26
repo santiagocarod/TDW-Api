975584a89df4c64ba7d4d98bc1c493efc38fceda
Santiago Caro <santiagocaroduque@gmail.com>
2020-04-26 13:43:25 +0200

    PersonRelationsController Implementado TEST OK!

--- src/Controller/Person/PersonRelationsController.php
+++ src/Controller/Person/PersonRelationsController.php
@@ -33,8 +33,14 @@ final class PersonRelationsController extends ElementRelationsBaseController
      */
     public function getEntities(Request $request, Response $response, array $args): Response
     {
-        // TODO
-        return Error::error($response, 501);
+        $elementData = [
+            'entityName' => Person::class,
+            'elementId' => $args['personId'],
+            'getter' => 'getEntities',
+            'stuff' => 'entities',
+        ];
+
+        return $this->getElements($response,$elementData);
     }
 
     /**
@@ -49,8 +55,15 @@ final class PersonRelationsController extends ElementRelationsBaseController
      */
     public function operationEntity(Request $request, Response $response, array $args): Response
     {
-        // TODO
-        return Error::error($response, 501);
+        $elementData = [
+            'entityName' => Person::class,
+            'elementId' => $args['personId'],
+            'stuffEName' => Entity::class,
+            'stuffId' => $args ['stuffId'],
+            'getter' => 'getEntities',
+            'stuff' => 'entities',
+        ];
+        return $this->operationStuff($request,$response,$elementData);
     }
 
     /**
@@ -63,8 +76,13 @@ final class PersonRelationsController extends ElementRelationsBaseController
      */
     public function getProducts(Request $request, Response $response, array $args): Response
     {
-        // TODO
-        return Error::error($response, 501);
+        $elementData = [
+            'entityName' => Person::class,
+            'elementId' => $args['personId'],
+            'getter' => 'getProducts',
+            'stuff' => 'products',
+        ];
+        return $this->getElements($response, $elementData);
     }
 
     /**
@@ -78,7 +96,14 @@ final class PersonRelationsController extends ElementRelationsBaseController
      */
     public function operationProduct(Request $request, Response $response, array $args): Response
     {
-        // TODO
-        return Error::error($response, 501);
+        $elementData = [
+            'entityName' => Person::class,
+            'elementId' => $args['personId'],
+            'stuffEName' => Product::class,
+            'stuffId' => $args['stuffId'],
+            'getter' => 'getProducts',
+            'stuff' => 'products',
+        ];
+        return $this->operationStuff($request, $response, $elementData);
     }
 }
